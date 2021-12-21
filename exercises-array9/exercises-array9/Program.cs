3c2e4eaa579525542f10dc2a3556d7428fb79efa
FarrukhSadikhov <farrukh.sadikhov@outlook.com>
2021-12-21 17:57:24 +0400

    new exercises

---
+++ exercises-array9/exercises-array9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exercises_array9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // 9) Отсортировать массив(пузырьком (Bubble) и почитать методы:  выбором(Select), вставками(Insert)) и попробовать отсортировать.
+
+        }
+    }
+}
